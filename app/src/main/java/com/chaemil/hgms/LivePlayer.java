7195dfcaaae74ed369d152df9608b5135ee4e206
Michal Mlejnek <chaemil72@gmail.com>
2014-12-09 20:01:56 +0100

    fixed live player fullscreen

--- app/src/main/java/com/chaemil/hgms/LivePlayer.java
+++ app/src/main/java/com/chaemil/hgms/LivePlayer.java
@@ -1,16 +1,23 @@
 package com.chaemil.hgms;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.chaemil.hgms.utils.Basic;
+import com.chaemil.hgms.utils.Utils;
 import com.chaemil.hgms.youtube.DeveloperKey;
 import com.chaemil.hgms.youtube.YouTubeFailureRecoveryActivity;
 import com.google.android.youtube.player.YouTubePlayer;
@@ -18,6 +25,9 @@ import com.google.android.youtube.player.YouTubePlayerView;
 
 import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+import static com.chaemil.hgms.utils.Utils.getScreenWidth;
+import static com.chaemil.hgms.utils.Utils.hideSystemUI;
+import static com.chaemil.hgms.utils.Utils.showSystemUI;
 
 /**
  * Created by chaemil on 2.12.14.
@@ -34,9 +44,10 @@ public class LivePlayer extends YouTubeFailureRecoveryActivity implements
     private YouTubePlayerView playerView;
     private YouTubePlayer player;
     private Button fullscreenButton;
-    private CompoundButton checkbox;
     private View otherViews;
     private String videoId;
+    private YouTubePlayer.PlayerStyle playerStyleMinimal = YouTubePlayer.PlayerStyle.MINIMAL;
+    private YouTubePlayer.PlayerStyle playerStyleDefault = YouTubePlayer.PlayerStyle.DEFAULT;
 
     private boolean fullscreen = true;
 
@@ -51,7 +62,9 @@ public class LivePlayer extends YouTubeFailureRecoveryActivity implements
 
 
 
-
+        if(getActionBar() != null) {
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
         setContentView(R.layout.live_player);
         baseLayout = (LinearLayout) findViewById(R.id.layout);
@@ -72,13 +85,14 @@ public class LivePlayer extends YouTubeFailureRecoveryActivity implements
     @Override
     public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player,
                                         boolean wasRestored) {
-        YouTubePlayer.PlayerStyle style = YouTubePlayer.PlayerStyle.MINIMAL;
+
         this.player = player;
         setControlsEnabled();
         // Specify that we want to handle fullscreen behavior ourselves.
         player.addFullscreenControlFlag(YouTubePlayer.FULLSCREEN_FLAG_CUSTOM_LAYOUT);
         player.setOnFullscreenListener(this);
-        player.setPlayerStyle(style);
+        player.setPlayerStyle(playerStyleMinimal);
+        player.setShowFullscreenButton(false);
         if (!wasRestored) {
             //videoId = videoId + "";
             player.cueVideo(String.valueOf(videoId));
@@ -107,7 +121,6 @@ public class LivePlayer extends YouTubeFailureRecoveryActivity implements
             // GONE and the player should be laid out across the whole screen.
             playerParams.width = LinearLayout.LayoutParams.MATCH_PARENT;
             playerParams.height = LinearLayout.LayoutParams.MATCH_PARENT;
-
             otherViews.setVisibility(View.GONE);
         } else {
             // This layout is up to you - this is just a simple example (vertically stacked boxes in
@@ -131,9 +144,21 @@ public class LivePlayer extends YouTubeFailureRecoveryActivity implements
         }
     }
 
+    public void screenOrientation()
+    {
+        Display getOrient = getWindowManager().getDefaultDisplay();
+        if (getOrient.getWidth() == getOrient.getHeight()) {
+        } else {
+            if (getOrient.getWidth() < getOrient.getHeight()) {
+                player.setPlayerStyle(playerStyleMinimal);
+
+            } else {
+                player.setPlayerStyle(playerStyleDefault);
+            }
+        }
+    }
+
     private void setControlsEnabled() {
-        /*checkbox.setEnabled(player != null
-                && getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT);*/
         fullscreenButton.setEnabled(player != null);
     }
 
@@ -146,7 +171,51 @@ public class LivePlayer extends YouTubeFailureRecoveryActivity implements
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
+        screenOrientation();
         doLayout();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.live_player_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch(id) {
+            case android.R.id.home:
+                finish();
+                Utils.goBackwardAnimation(this);
+                return true;
+            case R.id.action_share_link:
+                shareLink();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void shareLink() {
+        Intent share = new Intent(android.content.Intent.ACTION_SEND);
+        share.setType("text/plain");
+        share.putExtra(Intent.EXTRA_SUBJECT, getResources().getString(R.string.share_live_player));
+        share.putExtra(Intent.EXTRA_TEXT, "https://www.youtube.com/watch?v="
+                + String.valueOf(videoId)
+                + " " + getResources().getString(R.string.share_live_player_message));
+        startActivity(Intent.createChooser(share, getResources().getString(R.string.action_share)));
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+        Utils.goBackwardAnimation(this);
+    }
 }
